da632760741726b636152c8a3f05a043b5cd5c67
emiduran05 <emiduranf05@gmail.com>
2026-01-01 14:41:52 -0600

    testing

--- preplytool/vite.config.js
+++ preplytool/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx"
+      }
+    },
+    include: [
+      "quill",
+      "quill-image-resize-module-react",
+      "quill-better-table"
+    ]
+  }
+});
